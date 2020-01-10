01f6aa62a4cd6b4b31c8a69e2a3279ac71b31271
Rob Walker <rob@solana.com>
2020-01-10 05:17:02 -0800

    sanitize phrases (#9)
    * cargo fmt
    * sanitize phrases
    * fixups

--- tests/validate.rs
+++ tests/validate.rs
@@ -1,6 +1,6 @@
 extern crate bip39;
 
-use ::bip39::{Mnemonic, MnemonicType, Language};
+use bip39::{Language, Mnemonic, MnemonicType};
 
 fn validate_language(lang: Language) {
     let types = &[
@@ -29,6 +29,18 @@ fn validate_12_english() {
 }
 
 #[test]
+fn validate_12_english_extra_spaces() {
+    let phrase = " park remain  person kitchen mule spell knee armed position rail grid ankle ";
+    let clean_phrase = "park remain person kitchen mule spell knee armed position rail grid ankle";
+
+    let mnemonic = Mnemonic::from_phrase(phrase, Language::English).expect("Can create a Mnemonic");
+    let clean_mnemonic =
+        Mnemonic::from_phrase(clean_phrase, Language::English).expect("Can create a Mnemonic");
+
+    assert_eq!(mnemonic.entropy(), clean_mnemonic.entropy());
+}
+
+#[test]
 fn validate_15_english() {
     let phrase = "any paddle cabbage armor atom satoshi fiction night wisdom nasty they midnight chicken play phone";
 
@@ -42,14 +54,12 @@ fn validate_18_english() {
     let _ = Mnemonic::from_phrase(phrase, Language::English).expect("Can create a Mnemonic");
 }
 
-
 #[test]
 fn validate_21_english() {
     let phrase = "quality useless orient offer pole host amazing title only clog sight wild anxiety gloom market rescue fan language entry fan oyster";
 
     let _ = Mnemonic::from_phrase(phrase, Language::English).expect("Can create a Mnemonic");
 }
-
 
 #[test]
 fn validate_24_english() {
@@ -58,10 +68,10 @@ fn validate_24_english() {
     let _ = Mnemonic::from_phrase(phrase, Language::English).expect("Can create a Mnemonic");
 }
 
-
 #[test]
 fn validate_12_english_uppercase() {
-    let invalid_phrase = "Park remain person kitchen mule spell knee armed position rail grid ankle";
+    let invalid_phrase =
+        "Park remain person kitchen mule spell knee armed position rail grid ankle";
 
     assert!(Mnemonic::from_phrase(invalid_phrase, Language::English).is_err());
 }
